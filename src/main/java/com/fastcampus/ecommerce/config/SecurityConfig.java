41a010c96dbecb4402d665d66b4eb11f79ba8f38
LaurenDImas <dimas.yogi.777@gmail.com>
2025-10-20 15:08:06 +0700

    SERVICE AUTH

--- src/main/java/com/fastcampus/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/ecommerce/config/SecurityConfig.java
@@ -1,15 +1,38 @@
 package com.fastcampus.ecommerce.config;
 
+import com.fastcampus.ecommerce.service.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsImpl userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
     }
 }
--- src/main/java/com/fastcampus/ecommerce/config/middleware/GenericExceptionHandler.java
+++ src/main/java/com/fastcampus/ecommerce/config/middleware/GenericExceptionHandler.java
@@ -3,21 +3,27 @@ package com.fastcampus.ecommerce.config.middleware;
 import com.fastcampus.ecommerce.common.errors.*;
 import com.fastcampus.ecommerce.model.ErrorResponse;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
+import java.security.SignatureException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice // ← Tambah ini
+@ControllerAdvice
 @Slf4j
-//@Order(Ordered.LOWEST_PRECEDENCE)
-//@Profile("development")
 public class GenericExceptionHandler{
     @ExceptionHandler({
             ResourceNotFoundException.class,
@@ -45,8 +51,23 @@ public class GenericExceptionHandler{
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public @ResponseBody ErrorResponse handleGenericException(HttpServletRequest req, Exception exception){
+    public @ResponseBody ErrorResponse handleGenericException(
+            HttpServletRequest req,
+            HttpServletResponse resp,
+            Exception exception){
         log.error("Terjadi error, status code: {} error message: {}", HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        if (exception instanceof AccessDeniedException ||
+            exception instanceof SignatureException ||
+            exception instanceof ExpiredJwtException ||
+            exception instanceof AuthenticationException) {
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return ErrorResponse.builder()
+                    .code(HttpStatus.FORBIDDEN.value())
+                    .message(exception.getMessage())
+                    .timestamp(LocalDateTime.now())
+                    .build();
+        }
+        resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         return ErrorResponse.builder()
                 .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .message(exception.getMessage())
