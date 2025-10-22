c35c3df9bc0853348afa7e9fe161b7a1c111b649
LaurenDImas <dimas.yogi.777@gmail.com>
2025-10-22 11:38:48 +0700

    order reposiitory

---
+++ src/main/java/com/fastcampus/ecommerce/entity/UserAddress.java
@@ -0,0 +1,57 @@
+package com.fastcampus.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_addresses")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_address_id")
+    private Long userAddressId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "address_name", nullable = false)
+    private String addressName;
+
+    @Column(name = "street_address", nullable = false)
+    private String streetAddress;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private Boolean country;
+
+    @Column(name = "is_default", nullable = false)
+    private String isDefault;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
