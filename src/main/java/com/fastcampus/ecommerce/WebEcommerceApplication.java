080a914953089dda4573dc9639aa4104901f18f0
LaurenDImas <dimas.yogi.777@gmail.com>
2025-11-05 10:37:01 +0700

    implement github actions

--- src/main/java/com/fastcampus/ecommerce/WebEcommerceApplication.java
+++ src/main/java/com/fastcampus/ecommerce/WebEcommerceApplication.java
@@ -1,5 +1,6 @@
 package com.fastcampus.ecommerce;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +12,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class WebEcommerceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")              // Root project
+				.ignoreIfMissing()            // Tidak crash jika .env tidak ada
+				.systemProperties()           // Load ke System properties
+				.load();
+
 		SpringApplication.run(WebEcommerceApplication.class, args);
 	}
 
